57eb3cc7c5146a5e253cda0a4504fcc17cd21434
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-12 18:46:12 +0200

    Uninstalled  unecessary json packages.

--- ASP.NETCoreWebApplication/Contexts/InvestmentContext.cs
+++ ASP.NETCoreWebApplication/Contexts/InvestmentContext.cs
@@ -21,24 +21,24 @@ namespace ASP.NETCoreWebApplication.Contexts
                 new Investment
                 {
                     InvestmentId = 1, Username = "John", 
-                    Amount = 1000, Duration = 5 
+                    Amount = 1000, Duration = 5 , InvestmentReturns = 1000 * 5 * 0.3
                 }, 
                 new Investment
                 {
                     InvestmentId = 2, Username = "Mike", 
-                    Amount = 2000, Duration = 10 
+                    Amount = 2000, Duration = 10 , InvestmentReturns = 2000 * 10 * 0.3
                 },new Investment
                 {
                     InvestmentId = 3, Username = "Martha", 
-                    Amount = 4500, Duration = 6 
+                    Amount = 4500, Duration = 6 , InvestmentReturns = 4500 * 6 * 0.3
                 },new Investment
                 {
                     InvestmentId = 4, Username = "Janet", 
-                    Amount = 7500, Duration = 3 
+                    Amount = 7500, Duration = 3 , InvestmentReturns = 7500 * 3 * 0.3
                 },new Investment
                 {
                     InvestmentId = 5, Username = "Ron", 
-                    Amount = 1400, Duration = 7 ,
+                    Amount = 1400, Duration = 7 ,InvestmentReturns = 1400 * 7 * 0.3
                 }
             );
 
